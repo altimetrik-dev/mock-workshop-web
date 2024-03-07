f585561290f61b773da2047d07b240bd3e82e39c
Raul Gonzalez <rurbina@altimetrik.com>
2024-03-06 20:38:41 -0600

    implement vite

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,53 @@ function Input(props: InputProps) {
   return (
     <div className="flex flex-col">
       <label htmlFor={props.id}>{props.label}</label>
-      <input
-        type="text"
-        className="border border-gray-600 rounded p-2"
-        {...props}
-      />
+      <input type="text" className="border border-gray-200 p-1" {...props} />
     </div>
   );
 }
 
 function App() {
   return (
-    <div className="container">
-      <h1 className="text-xl text-blue-800">Company</h1>
-      <div className="flex">
-        <Input
-          id="name-of-the-legal-entity"
-          placeholder="Enter the name of the legal entity"
-          label="Name of the legal entity"
-        />
-        <div className="flex">
-          <div className="flex flex-row">
-            <Input
-              id="client-id"
-              label="Client ID"
-              placeholder="Enter the clients IDs"
-            />
+    <div className="container flex w-full">
+      <div className="shadow rounded-xl border p-2">
+        <h1 className="text-xl text-blue-800">Company</h1>
+        <div className="flex flex-col">
+          <Input
+            id="name-of-the-legal-entity"
+            placeholder="Enter the name of the legal entity"
+            label="Name of the legal entity"
+          />
+          <div className="flex flex-row gap-2">
+            <div className="flex flex-col">
+              <Input
+                id="client-id"
+                label="Client ID"
+                placeholder="Enter the clients IDs"
+              />
+            </div>
+            <div className="flex flex-col">
+              <Input
+                id="mc-number"
+                label="MC Number"
+                placeholder="Enter the MC Numbers"
+              />
+            </div>
+          </div>
+          <div className="flex flex-row gap-2">
+            <div className="flex flex-col">
+              <Input
+                id="group-id"
+                label="Group ID"
+                placeholder="Enter the group IDs"
+              />
+            </div>
+            <div className="flex flex-col">
+              <Input
+                id="brand-id"
+                label="Brand ID"
+                placeholder="Enter the Brand IDs"
+              />
+            </div>
           </div>
         </div>
       </div>
